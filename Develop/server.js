3536c65354190d0e29930ee9bc3ab9a4543e1175
Clarisagonzalez <136874433+Clarisagonzalez@users.noreply.github.com>
2023-12-01 16:10:46 -0500

    fixed .env issue + rest of code

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,11 @@
+require('dotenv').config();
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+
 const express = require('express');
 const routes = require('./routes');
+const sequelize = require('./config/connection');
 // import sequelize connection
 
 const app = express();
@@ -11,6 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
-});
+})});
